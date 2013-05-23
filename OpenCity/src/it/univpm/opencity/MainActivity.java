d778a6df1434f66f30d740ae29a2fdf2777e05b9
Mattia Bugossi <mattiabugossi@gmail.com>
2013-05-23 15:04:44 +0200

    Mattia

--- OpenCity/src/it/univpm/opencity/MainActivity.java
+++ OpenCity/src/it/univpm/opencity/MainActivity.java
@@ -1,45 +1,108 @@
 package it.univpm.opencity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.plus.PlusClient;
+import com.google.android.gms.plus.PlusClient.Builder;
+import com.google.android.gms.plus.PlusShare;
 
-
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ConnectionCallbacks,
+		OnConnectionFailedListener,OnClickListener {
+	private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+
+	private ProgressDialog mConnectionProgressDialog;
+	private PlusClient mPlusClient;
+	private ConnectionResult mConnectionResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//DA ELIMINARE POI!!
+
+		mPlusClient = new PlusClient.Builder(this, this, this)
+				.setVisibleActivities("http://schemas.google.com/AddActivity",
+						"http://schemas.google.com/BuyActivity").build();
+		// Progress bar to be displayed if the connection failure is not
+		// resolved.
+		mConnectionProgressDialog = new ProgressDialog(this);
+		mConnectionProgressDialog.setMessage("Signing in...");
+
+		findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+		// DA ELIMINARE POI!!
 		Button btnGotoCameraActivity = (Button) findViewById(R.id.btnGoToCamera);
 		btnGotoCameraActivity.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v) {		
+			public void onClick(View v) {
 				Intent intent = new Intent(MainActivity.this,
 						CameraActivity.class);
 				startActivity(intent);
 			}
 		});
-		
+
 		Button btnGotoMapActivity = (Button) findViewById(R.id.btnGoToMap);
 		btnGotoMapActivity.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v) {		
-				Intent intent = new Intent(MainActivity.this,
-						MapActivity.class);
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, MapActivity.class);
 				startActivity(intent);
 			}
 		});
-		///
+
+		Button shareButton = (Button) findViewById(R.id.share_button);
+		shareButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Launch the Google+ share dialog with attribution to your app.
+				Intent shareIntent = new PlusShare.Builder()
+						.setType("text/plain")
+						.setText("Welcome to the Google+ platform.")
+						.setContentUrl(
+								Uri.parse("https://developers.google.com/+/"))
+						.getIntent();
+
+				startActivityForResult(shareIntent, 0);
+			}
+		});
+
+		// /
+	}
+@Override
+	public void onClick(View view) {
+		
+	
+	if (view.getId() == R.id.sign_in_button && !mPlusClient.isConnected()) {
+		mPlusClient.connect();
+		
+		/*if (mConnectionResult == null) {
+				mConnectionProgressDialog.show();
+			} else {
+				try {
+					mConnectionResult.startResolutionForResult(this,
+							REQUEST_CODE_RESOLVE_ERR);
+				} catch (SendIntentException e) {
+					// Try connecting again.
+					mConnectionResult = null;
+					mPlusClient.connect();
+				}		}*/
+		}
 	}
 
 	@Override
@@ -47,6 +110,56 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		//mPlusClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mPlusClient.disconnect();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (result.hasResolution()) {
+			try {
+				result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+			} catch (SendIntentException e) {
+				mPlusClient.connect();
+			}
+		}
+		// Save the result and resolve the connection failure upon a user click.
+		mConnectionResult = result;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode,
+			Intent intent) {
+		if (requestCode == REQUEST_CODE_RESOLVE_ERR
+				&& responseCode == RESULT_OK) {
+			mConnectionResult = null;
+			mPlusClient.connect();
+		}
+	}
+
+	@Override
+	public void onConnected() {
+		String accountName = mPlusClient.getAccountName();
+		Toast.makeText(this, accountName + " is connected.", Toast.LENGTH_LONG)
+				.show();
+		mConnectionProgressDialog.dismiss();
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+
+	}
+
+	@Override
+	public void onDisconnected() {
+		Log.d("opencity", "disconnected");
 	}
 
 }
